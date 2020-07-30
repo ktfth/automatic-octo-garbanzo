ee388fd68b512d95d59425a40a79d21092f394fd
Kaique Silva <kaeyosthaeron@gmail.com>
2020-07-30 17:13:19 -0300

    Integrated with the api

--- public/fetch_url.js
+++ public/fetch_url.js
@@ -1,5 +1,17 @@
 'use strict';
 
+// Socket.io is exposed as the `io` global.
+const socket = io('http://localhost:3030');
+// @feathersjs/client is exposed as the `feathers` global.
+const app = feathers();
+
+// Set up Socket.io client with the socket
+app.configure(feathers.socketio(socket));
+
+// Receive real-time events through Socket.io
+app.service('v1/urls')
+  .on('created', message => console.log('New message created', message));
+
 const e = React.createElement;
 
 class URLInput extends React.Component {
@@ -7,7 +19,7 @@ class URLInput extends React.Component {
     super(props);
     this.handleChange = this.handleChange.bind(this);
     this.handleKeyDown = this.handleKeyDown.bind(this);
-    this.state = {url: ''};
+    this.state = { url: '' };
   }
 
   handleChange(e) {
@@ -17,6 +29,9 @@ class URLInput extends React.Component {
   handleKeyDown(e) {
     if (e.keyCode === 13) {
       console.log('Trigger action: ' + this.state.url);
+      app.service('v1/urls').create({
+        text: this.state.url
+      });
     }
   }
 
